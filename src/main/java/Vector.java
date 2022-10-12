37a89fe8d68f4d23121e64dc7891371f737e4792
IvanSvitavskiy <ivan.svitavskiy@mail.ru>
2022-10-12 22:05:54 +0300

    Revert "solved"
    This reverts commit b636189bfef61d3e51e0329ed0f8645e76376b1e.

--- src/main/java/Vector.java
+++ src/main/java/Vector.java
@@ -1,73 +1,32 @@
-import java.util.Arrays;
-import java.util.Objects;
-
-public class Vector {
-    private double x = 0;
-    private double y = 0;
-    private double z = 0;
-
-
-    Vector(double x, double y, double z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
-
+public class Vector
+{
     // Длина вектора. Корень из суммы квадратов
-    public double length() {
-        return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
+    double length(){
+
     }
 
     // метод, вычисляющий скалярное произведение
-    public double scalarProduct(Vector vector) {
-        return this.x * vector.x + this.y * vector.y + this.z * vector.z;
+    double scalarProduct(Vector vector){
+
     }
 
     // метод, вычисляющий векторное произведение
-    public Vector crossProduct(Vector vector) {
-        double x1 = this.y * vector.z - this.z * vector.y;
-        double y1 = this.z * vector.x - this.x * vector.z;
-        double z1 = this.x * vector.y - this.y * vector.x;
-        return new Vector(x1, y1, z1);
+    Vector crossProduct(Vector vector){
+
     }
 
     // Косинус между двумя векторами
-    public double cos(Vector vector) {
-        return scalarProduct(vector) / (this.length() * vector.length());
+    double cos(Vector vector){
+
     }
 
     // Сумма двух векторов
-    public Vector add(Vector vector) {
-        double x1 = this.x + vector.x;
-        double y1 = this.y + vector.y;
-        double z1 = this.z + vector.z;
-        return new Vector(x1, y1, z1);
+    Vector add(Vector vector){
+
     }
 
     // Разность двух веторов
-    public Vector subtract(Vector vector) {
-        double x1 = this.x - vector.x;
-        double y1 = this.y - vector.y;
-        double z1 = this.z - vector.z;
-        return new Vector(x1, y1, z1);
-    }
+    Vector subtract(Vector vector){
 
-    @Override
-    public int hashCode() {
-        return Arrays.hashCode(new Object[]{this.x, this.y, this.z});
-    }
-
-    public boolean equals(Vector vector) {
-        if (vector == this) return true;
-        if (vector== null || vector.getClass()!= this.getClass()){
-            return false;
-        }
-        return this.x==vector.x && this.y==vector.y && this.z== vector.z;
-    }
-
-    public void print(){
-        System.out.print(this.x + " ");
-        System.out.print(this.y + " ");
-        System.out.println(this.z);
     }
 }
